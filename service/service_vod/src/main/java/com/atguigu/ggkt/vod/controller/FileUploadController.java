13961264a973d2188d782d8ce005646a9a5b1d49
13232160100 <ypc0100@qq.com>
2022-11-13 14:55:46 +0800

    233

--- service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/FileUploadController.java
+++ service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/FileUploadController.java
@@ -2,13 +2,17 @@ package com.atguigu.ggkt.vod.controller;
 
 import com.atguigu.ggkt.result.Result;
 import com.atguigu.ggkt.vod.service.FileService;
+import com.atguigu.ggkt.vod.utils.StaticGetPrivate;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Api(tags = "文件上传接口")
@@ -24,6 +28,21 @@ public class FileUploadController {
     @PostMapping("upload")
     public Result uploadFile(MultipartFile file) {
         String url = fileService.upload(file);
+        return Result.ok(url).message("上传文件成功");
+    }
+
+    @ApiOperation("文件上传")
+    @PostMapping("fileUpload")
+    public Result fileUpload(@RequestParam("file") MultipartFile file) {
+        String url = StaticGetPrivate.getKkAddres() + "/fileUpload";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("file", file.getResource());
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+
+        StaticGetPrivate.getTemplates().exchange(url, HttpMethod.POST, requestEntity, String.class);
         return Result.ok(url).message("上传文件成功");
     }
 }
--- service/service_vod/src/main/java/com/atguigu/ggkt/vod/utils/StaticGetPrivate.java
+++ service/service_vod/src/main/java/com/atguigu/ggkt/vod/utils/StaticGetPrivate.java
@@ -3,6 +3,7 @@ package com.atguigu.ggkt.vod.utils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
@@ -16,16 +17,27 @@ public class StaticGetPrivate {
     private static StaticGetPrivate staticGetPrivate;
 
     @Resource
+    private RestTemplate restTemplate;
+
+    @Resource
     private JavaMailSender javaMailSender;
 
     @Value("${spring.mail.username}")
     private String emailFrom;
 
+    @Value("${kkFile.address}")
+    private String kkAddres;
+
     @PostConstruct
     public void init() {
         staticGetPrivate = this;
+        staticGetPrivate.restTemplate = this.restTemplate;
         staticGetPrivate.javaMailSender = this.javaMailSender;
         staticGetPrivate.emailFrom = this.emailFrom;
+    }
+
+    public static RestTemplate getTemplates() {
+        return staticGetPrivate.restTemplate;
     }
 
     public static JavaMailSender getMailSender() {
@@ -34,5 +46,9 @@ public class StaticGetPrivate {
 
     public static String getEmailFrom() {
         return staticGetPrivate.emailFrom;
+    }
+
+    public static String getKkAddres() {
+        return staticGetPrivate.kkAddres;
     }
 }
